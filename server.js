79a07a166c89212a6edf57704b28c6b1e44e0c80
alemoya <alejandro5320@gmail.com>
2020-09-18 12:20:53 -0300

    Exercise resolved

--- server.js
+++ server.js
@@ -1,39 +1,100 @@
-// server.js
-// where your node app starts
+'use strict';
 
-// init project
 var express = require('express');
+var mongoose = require('mongoose');
+var shortId = require('shortid');
+var bodyParser = require('body-parser');
+var validUrl = require('valid-url');
+var cors = require('cors');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+var port = process.env.PORT || 3000;
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+const url = 'mongodb+srv://user:user@cluster0.v3sgu.mongodb.net/course?retryWrites=true&w=majority';
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000
+});
+mongoose.connection.once('open', () => {
+  console.log("Connection successfully")
 });
 
+const urlShortener = mongoose.model("urlShortener", new mongoose.Schema({
+  realUrl: String,
+  shortUrl: String
+}));
 
-// your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cors());
+app.use(express.json());
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.post('/api/shorturl/new', async (req, res) => {
+
+  const url = req.body.url_input;
+  const urlCode = shortId.generate();
+
+  // Its valid the url ?
+  if (!validUrl.isWebUri(url)) {
+    res.status(401).json({
+      error: 'Invalid URL'
+    })
+  } else {
+    try {
+      // Existed in the db ?
+      let findOne = await urlShortener.findOne({
+        original_url: url
+      })
+      if (findOne) {
+        res.json({
+          original_url: findOne.original_url,
+          short_url: findOne.short_url
+        })
+      } else {
+        // Otherwise create new an return this
+        findOne = new URL({
+          original_url: url,
+          short_url: urlCode
+        })
+        await findOne.save()
+        res.json({
+          original_url: findOne.original_url,
+          short_url: findOne.short_url
+        })
+      }
+    } catch (err) {
+      console.error(err)
+      res.status(500).json('Error in server')
+    }
+  }
+})
+
+app.get('/api/shorturl/:short_url?',
+    async (req, res) => {
+      try {
+        const urlParams = await urlShortener.findOne({
+          short_url: req.params.short_url
+        })
+        if (urlParams) {
+          return res.redirect(urlParams.original_url)
+        } else {
+          return res.status(404).json('No URL found')
+        }
+      } catch (err) {
+        console.log(err)
+        res.status(500).json('Server error')
+      }
+    }
+)
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.get("/api/whoami", function (req, res) {
-  var ips = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  res.json({
-    ipaddress: ips,
-    language: req.headers['accept-language'],
-    software: req.headers['user-agent']});
-});
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(port, function () {
+  console.log('Node.js listening in ', port + '...');
 });
